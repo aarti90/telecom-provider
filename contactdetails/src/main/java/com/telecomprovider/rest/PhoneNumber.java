dc3ccd5997e0d693e84e0c5637e2f65ed6870b33
aarti90 <rtkatariya0@gmail.com>
2019-02-14 23:13:17 +0530

    New Project

---
+++ contactdetails/src/main/java/com/telecomprovider/rest/PhoneNumber.java
@@ -0,0 +1,22 @@
+package com.telecomprovider.rest;
+
+public class PhoneNumber {
+
+	private String phNumber;
+	private String active;
+	
+	public String getPhNumber() {
+		return phNumber;
+	}
+	public void setPhNumber(String phNumber) {
+		this.phNumber = phNumber;
+	}
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	
+}
